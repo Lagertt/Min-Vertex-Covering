541bf1895871f9f8f0204336a3db9ccb4444b810
Polina Ryabova <polinaryabova.2@gmail.com>
2023-11-24 01:02:47 +0500

    я всё поменяла теперь всё работает

--- main.py
+++ main.py
@@ -1,121 +1,104 @@
-import random
-import time
+import random 
+from time import time
+import copy
+
+#         	Задача о вершинном покрытии минимальной мощности.
+# Дано: число вершин в графе (n) и сам граф (неориентированный) в виде матрицы смежности.
+# Получить: количество вершин в найденном вершинном покрытии, а также сам список вершин.
 
 def read_graph_from_file(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
         n = int(lines[0].strip())
-        matrix = [list(map(int, line.strip().split())) for line in lines[1:]]
+        graph = [list(map(int, line.strip().split())) for line in lines[1:]]
 
-    # Преобразуем матрицу смежности в список рёбер
-    graph = []
+    return n, graph
+
+def generateGraph(n):
+  graph = [[0] * n for _ in range(n)]
+
+  for i in range(n):
+    for j in range(n):
+      if (i != j):
+        if (i < j):
+          graph[i][j] = int(random.choice([1, 0]))
+        else:
+          graph[i][j] = graph[j][i]
+    
+  return graph
+
+# Полный перебор
+def fullSearchAlg(n, graph):
+  def is_vertex_cover(graph, vertices):
     for i in range(n):
-        for j in range(i + 1, n):
-            if matrix[i][j] == 1 or matrix[j][i] == 1:
-                graph.append((i + 1, j + 1))
+      for j in range(n):
+        if graph[i][j] == 1 and i not in vertices and j not in vertices:
+          return False
+    return True
 
-    return n, graph
+  result = []
+  min_size = n
 
-def generate_random_graph(n):
-    # Генерация случайного графа с n вершинами
-    graph = []
-    for i in range(1, n + 1):
-        for j in range(i + 1, n + 1):
-            # Включаем ребро с вероятностью 0.5 (можно настроить по желанию)
-            if random.choice([True, False]):
-                graph.append((i, j))
-    return n, graph
+  for i in range(2 ** n):
+    vertex_set = []
+    for j in range(n):
+      if i & (1 << j):
+        vertex_set.append(j)
 
-#переборный алгоритм
-def find_vertex_cover(n, graph):
-    min_vertex_cover_size = float('inf')
-    min_vertex_cover = []
+    if is_vertex_cover(graph, vertex_set) and len(vertex_set) < min_size:
+      result = vertex_set
+      min_size = len(vertex_set)
 
-    for i in range(2**n):
-        current_vertex_cover = []
-        for j in range(n):
-            if (i >> j) & 1:
-                current_vertex_cover.append(j + 1)
-
-        is_vertex_cover = True
-        for edge in graph:
-            if edge[0] not in current_vertex_cover and edge[1] not in current_vertex_cover:
-                is_vertex_cover = False
-                break
-
-        if is_vertex_cover and len(current_vertex_cover) < min_vertex_cover_size:
-            min_vertex_cover_size = len(current_vertex_cover)
-            min_vertex_cover = current_vertex_cover
-
-    return min_vertex_cover_size, min_vertex_cover
-
-# Приближенный алгоритм
-def approxAlg(n, graph):    
-    result = []  # список для хранения вершинного покрытия
-    graph_matrix = [[0] * n for _ in range(n)]  # Создаем нулевую матрицу смежности
-
-    for edge in graph:
-        graph_matrix[edge[0] - 1][edge[1] - 1] = 1
-        graph_matrix[edge[1] - 1][edge[0] - 1] = 1
-
-    while any(any(x == 1 for x in row) for row in graph_matrix):  # пока в графе есть непокрытые ребра
-        edge_found = False
-        # Шаг 1: Выбрать произвольное ребро
-        for i in range(n):
-            for j in range(n):
-                if graph_matrix[i][j] == 1:
-                    edge_found = True
-                    # Шаг 2: Добавить к результату оба конца этого ребра
-                    result.append(i + 1)
-                    result.append(j + 1)
-                    # Шаг 3: Удалить все ребра, инцидентные этим вершинам
-                    for k in range(n):
-                        graph_matrix[i][k] = 0
-                        graph_matrix[k][i] = 0
-                        graph_matrix[j][k] = 0
-                        graph_matrix[k][j] = 0
-                    break  # Перейти к следующей итерации
-        if not edge_found:
-            break  # Если не найдено ребро, выходим из цикла
-
-    return len(set(result)), list(set(result)) # возвращаем результат
+  return len(result), result 
 
 # Жадный алгоритм
 def greedyAlg(n, graph):
-    result = []  # список для хранения вершинного покрытия
-    graph_matrix = [[0] * n for _ in range(n)]  # Создаем нулевую матрицу смежности
+  result = []
+  while any(any(x == 1 for x in row) for row in graph): 
+    max_degree = 0 
+    max_vertex = None 
 
-    for edge in graph:
-        graph_matrix[edge[0] - 1][edge[1] - 1] = 1
-        graph_matrix[edge[1] - 1][edge[0] - 1] = 1
+    for i in range(n): 
+      degree = 0 
+      for j in range(n): 
+        if graph[i][j] == 1: 
+          degree += 1 
+      if degree > max_degree: 
+        max_degree = degree 
+        max_vertex = i
 
-    while any(any(x == 1 for x in row) for row in graph_matrix):  # пока в графе есть непокрытые ребра
-        max_degree = 0  # переменная для хранения максимальной степени вершины
-        max_vertex = None  # переменная для хранения вершины с максимальной степенью
+    if max_vertex is None: 
+      return result 
+    
+    result.append(max_vertex) 
 
-        for i in range(n):  # проходим по всем вершинам графа
-            degree = 0  # переменная для хранения степени текущей вершины
-            for j in range(n):  # проходим по всем вершинам, инцидентным текущей
-                if graph_matrix[i][j] == 1:  # если между текущими вершинами есть ребро
-                    degree += 1  # увеличиваем степень текущей вершины на 1
-            if degree > max_degree:  # если степень текущей вершины больше максимальной
-                max_degree = degree  # обновляем значение максимальной степени
-                max_vertex = i  # обновляем значение вершины с максимальной степенью
+    for i in range(n):
+      if graph[max_vertex][i] == 1: 
+        graph[max_vertex][i] = 0
+        graph[i][max_vertex] = 0
 
-        if max_vertex is None:  # если не удалось найти вершину с максимальной степенью
-            end_time3 = time.time()  # Засекаем процессорное время окончания выполнения
-            execution_time_greedy = end_time3\
-                             - start_time3
-            return len(result), result, execution_time_greedy # возвращаем текущий результат
+  return len(result), result 
 
-        result.append(max_vertex + 1)  # добавляем вершину с максимальной степенью к результату
+#Приближенный алгоритм
+def approxAlg(n, graph):  
+  result = set()  # Множество вершин покрытия
+    
+  while any(any(x == 1 for x in row) for row in graph):
+    u, v = random.sample(range(n), 2)
 
-        for i in range(n):  # проходим по всем вершинам, инцидентным выбранной вершине
-            if graph_matrix[max_vertex][i] == 1:  # если есть ребро между выбранной и текущей вершиной
-                graph_matrix[max_vertex][i] = 0  # удаляем это ребро из графа
-                graph_matrix[i][max_vertex] = 0  # удаляем это ребро из графа (граф неориентированный)
+    result.add(u)
+    result.add(v)
 
-    return len(result), result # возвращаем результат
+    for i in range(n):
+      if graph[u][i] == 1:
+        graph[u][i] = 0
+        graph[i][u] = 0
+      if graph[v][i] == 1:
+        graph[v][i] = 0
+        graph[i][v] = 0
+
+  return len(result), list(result)
+
 
 if __name__ == "__main__":
     choice = input("Источник ввода (1.file/2.random): ").lower()
@@ -123,32 +106,33 @@ if __name__ == "__main__":
     if choice == '1':
         filename = input("Введите имя файла с матрицей смежности: ")
         n, graph = read_graph_from_file(filename)
+        print(graph)
     elif choice == '2':
         n = int(input("Введите количество вершин в графе: "))
-        n, graph = generate_random_graph(n)
+        graph = generateGraph(n)
 
     # Результаты метода полного перебора
-    start_time = time.time()  # Засекаем процессорное время начала выполнения
-    result_size, result_vertex_cover = find_vertex_cover(n, graph)
-    end_time = time.time()  # Засекаем процессорное время окончания выполнения
+    start_time = time()  # Засекаем процессорное время начала выполнения
+    result_size, result_vertex_cover = fullSearchAlg(n, copy.deepcopy(graph))
+    end_time = time()  # Засекаем процессорное время окончания выполнения
     print("\nМетод полного перебора:")
     print("Размер вершинного покрытия:", result_size)
     print("Вершины вершинного покрытия:", result_vertex_cover)
     print("Время выполнения:", end_time - start_time, "секунд")
 
     # Результаты приближенного алгоритма
-    start_time = time.time()  # Засекаем процессорное время начала выполнения
-    result_size_approx, result_vertex_cover_approx = approxAlg(n, graph.copy())
-    end_time = time.time()  # Засекаем процессорное время окончания выполнения
+    start_time = time()  # Засекаем процессорное время начала выполнения
+    result_size_approx, result_vertex_cover_approx = approxAlg(n, copy.deepcopy(graph))
+    end_time = time()  # Засекаем процессорное время окончания выполнения
     print("\nПриближенный алгоритм:")
     print("Размер вершинного покрытия:", result_size_approx)
     print("Вершины вершинного покрытия:", result_vertex_cover_approx)
     print("Время выполнения:", end_time - start_time, "секунд")
 
     # Результаты жадного алгоритма
-    start_time3 = time.time()  # Засекаем процессорное время начала выполнения
-    result_size_greedy, result_vertex_cover_greedy = greedyAlg(n, graph.copy())
-    end_time3 = time.time()  # Засекаем процессорное время окончания выполнения
+    start_time = time()  # Засекаем процессорное время начала выполнения
+    result_size_greedy, result_vertex_cover_greedy = greedyAlg(n, copy.deepcopy(graph))
+    end_time = time()  # Засекаем процессорное время окончания выполнения
     print("\nЖадный алгоритм:")
     print("Размер вершинного покрытия:", result_size_greedy)
     print("Вершины вершинного покрытия:", result_vertex_cover_greedy)
